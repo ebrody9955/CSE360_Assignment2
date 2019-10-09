63651ac0bc219d589f761099e177441852a86dfd
Ethan Brody <Ethan Brody@DESKTOP-DDROF61>
2019-10-09 01:51:32 -0700

    Initial commit of CSE 360 assignment 2.

---
+++ CSE360_Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,52 @@
+/*
+ * Ethan Brody
+ * CSE 360
+ * Assignment 2 AddingMachine
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/*
+	 * No Changes Made
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * No Changes Made
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * No Changes Made
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * No Changes Made
+	 */	
+	public String toString () {
+		return "";
+	}
+	
+	/*
+	 * No Changes Made
+	 */
+	public void clear() {
+	
+	}
+	
+}
